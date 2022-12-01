b19acff4cd5c46db26717fefa2a3590cfe134812
KomaNatoR <108286639+KomaNatoR@users.noreply.github.com>
2022-12-01 18:45:55 +0000

    Deploying to gh-pages from @ KomaNatoR/goit-js-hw-10@eed97e46eb894ddada2324b9a3344f4c58441360 🚀

--- index.244a8357.js
+++
@@ -1,2 +0,0 @@
-function t(t){return fetch(`https://restcountries.com/v3.1/name/${t}?fields=name.official,capital,population,flags,languages`).then((t=>t.json()))}({inputEl:document.querySelector("input"),listEl:document.querySelector(".country-list")}).inputEl.addEventListener("input",(function(e){const n=e.currentTarget.value;t(n.trim()),console.dir(t(n))}));
-//# sourceMappingURL=index.244a8357.js.map
