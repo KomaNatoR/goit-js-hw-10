59097aa03f1acc290962d3097cdf68391d06f7c9
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-12-02 10:41:25 +0200

    big country
    created template for one country

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
-import { smallCountryTemplate } from './js/templates';
+import { smallCountryTemplate, bigCountryTemplate } from './js/templates';
 import Notiflix from 'notiflix';
 // import debounce from 'lodash.debounce';
 
@@ -9,6 +9,7 @@ const DEBOUNCE_DELAY = 300;
 const refs = {
     inputEl: document.querySelector('input'),
     listEl: document.querySelector('.country-list'),
+    containerEl: document.querySelector('.country-info'),
 };
 
 // refs.inputEl.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
@@ -16,16 +17,22 @@ refs.inputEl.addEventListener('input',onInput);
 
 function onInput(e) {
     const inputData = e.currentTarget.value;
-    // let responseData = [];
+    if (inputData.length===0) {
+        refs.listEl.innerHTML ="";    
+    }
 
     fetchCountries(inputData.trim()).then(createCountriesTemplate);
 };
 
 function createCountriesTemplate(countries) {
     if (countries.length > 10) return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.!!!');
-    
-    
+    if (countries.length === 1) {
+        refs.listEl.innerHTML ="";
+        return refs.containerEl.innerHTML=bigCountryTemplate();
+    };
+
+    const arrayOfCountryes = countries.map(country=>smallCountryTemplate(country));
+    refs.listEl.innerHTML=arrayOfCountryes.join('');    
     console.log(countries);
 };
 
-refs.listEl.innerHTML=smallCountryTemplate();
--- src/js/templates.js
+++ src/js/templates.js
@@ -1,5 +1,17 @@
-export const smallCountryTemplate = () =>
+export const smallCountryTemplate = ({flags,altSpellings}) =>
     `<li style="display:flex; gap:5px;">
-        <img src="https://flagcdn.com/pe.svg" alt="Republic of Peru" width="30">
-        <span>Republic of Peru</span>
+        <img src="${flags.svg}" alt="${altSpellings[1]}" width="32">
+        <span>${altSpellings[1]}</span>
     </li>`;
+
+export const bigCountryTemplate = () => `
+    <ul style=" padding-left: 0;">
+    <li style="display:flex; gap:5px;">
+        <img src="https://flagcdn.com/ua.svg" alt="Ukraine" width="32">
+        <span style="font-size: 25px;font-weight: bold;">Ukraine</span>
+    </li>
+    </ul>
+    <p><span class="par">Capital:</span> Kyiv</p>
+    <p><span class="par">Population:</span> 52000000</p>
+    <p><span class="par">Languages:</span> Ukrainian</p>
+`;
