fd92d2cf5b9981484db0af5e8cc8e0e02ba05356
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-12-02 14:26:33 +0200

    maybe it`s done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import { smallCountryTemplate, bigCountryTemplate } from './js/templates';
 import Notiflix from 'notiflix';
-// import debounce from 'lodash.debounce';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -12,27 +12,36 @@ const refs = {
     containerEl: document.querySelector('.country-info'),
 };
 
-// refs.inputEl.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
-refs.inputEl.addEventListener('input',onInput);
+refs.inputEl.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
+// refs.inputEl.addEventListener('input',onInput);
 
 function onInput(e) {
-    const inputData = e.currentTarget.value;
+    const inputData = e.target.value;
     if (inputData.length===0) {
         refs.listEl.innerHTML ="";    
     }
 
-    fetchCountries(inputData.trim()).then(createCountriesTemplate);
+    fetchCountries(inputData.trim()).then(createCountriesTemplate).catch(error);
 };
 
 function createCountriesTemplate(countries) {
-    if (countries.length > 10) return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.!!!');
+    if (countries.length === 0) return;
+    if (countries.length > 10) {
+        refs.listEl.innerHTML = "";
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.!!!')
+    };
     if (countries.length === 1) {
-        refs.listEl.innerHTML ="";
-        return refs.containerEl.innerHTML=bigCountryTemplate();
+        refs.listEl.innerHTML = "";
+        return refs.containerEl.innerHTML=bigCountryTemplate(...countries);
     };
 
-    const arrayOfCountryes = countries.map(country=>smallCountryTemplate(country));
+    const arrayOfCountryes = countries.map(country => smallCountryTemplate(country));
+    refs.containerEl.innerHTML = "";
     refs.listEl.innerHTML=arrayOfCountryes.join('');    
     console.log(countries);
 };
+
+function error() {
+    return Notiflix.Notify.failure('Oops, there is no country with that name');
+}
 
--- src/js/templates.js
+++ src/js/templates.js
@@ -4,14 +4,14 @@ export const smallCountryTemplate = ({flags,altSpellings}) =>
         <span>${altSpellings[1]}</span>
     </li>`;
 
-export const bigCountryTemplate = () => `
+export const bigCountryTemplate = ({flags,altSpellings,capital,languages,population}) => `
     <ul style=" padding-left: 0;">
     <li style="display:flex; gap:5px;">
-        <img src="https://flagcdn.com/ua.svg" alt="Ukraine" width="32">
-        <span style="font-size: 25px;font-weight: bold;">Ukraine</span>
+        <img src="${flags.svg}" alt="${altSpellings[1]}" width="32">
+        <span style="font-size: 25px;font-weight: bold;">${altSpellings[1]}</span>
     </li>
     </ul>
-    <p><span class="par">Capital:</span> Kyiv</p>
-    <p><span class="par">Population:</span> 52000000</p>
-    <p><span class="par">Languages:</span> Ukrainian</p>
+    <p><span style="font-weight: bold;">Capital:</span> ${capital}</p>
+    <p><span style="font-weight: bold;">Population:</span> ${population}</p>
+    <p><span style="font-weight: bold;">Languages:</span> ${Object.values(languages).join(", ")}</p>
 `;
